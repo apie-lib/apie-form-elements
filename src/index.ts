90c99d841ba983c713ef7ee33a8d8d841536edc5
Pieter Jordaan <pieter_jordaan@hotmail.com>
2024-09-21 15:22:08 +0200

    add fallback renderer as export

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export type { Components, JSX } from './components';
 export * from './utils/utils';
 export * from './utils/RenderInfo';
+export { FallbackRenderInfo }  from './utils/FallbackRenderInfo';
